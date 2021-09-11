c113ca7d55f9d63587d4cb729a15dd5787215f32
Vinícius Amorim <viniciusamorim80@gmail.com>
2021-09-11 11:39:05 -0300

    Update Yuri Khristich Solution.jsx

--- Yuri Khristich Solution.jsx
+++ Yuri Khristich Solution.jsx
@@ -10,7 +10,18 @@ dialog.add("statictext", undefined, "Last copied text:");
 
 var msg = dialog.add("edittext");
 msg.text = "";
-msg.characters = 40
+msg.characters = 40;
+
+// --- here is the Pause button
+var pause = false;
+var pause_btn = dialog.add("button", [10, 10, 300, 30], "Pause");
+pause_btn.onClick = function () {
+    pause = !pause;
+    if (pause) msg.text = "[PAUSED] " + msg.text; // it can be done better, but it works, too
+    if (!pause) msg.text = msg.text.replace(/^\[PAUSED\] /, "");
+    pause_btn.text = pause ? "Resume" : "Pause";
+};
+// ---
 
 var doc = app.activeDocument;
 var master_frame = app.selection[0]; // <-- the Text Script frame
@@ -26,10 +37,11 @@ if (master_frame instanceof TextFrame || master_frame instanceof Story) {
 
 function selectionChanged() {
 
+    if (pause) return; // <-- here you check the state of the button
+
     var sel = doc.selection[0];
 
     if (sel instanceof InsertionPoint) sel = sel.parent.textContainers[0];
-
     if (sel instanceof TextFrame || sel instanceof Story) {
 
         if (sel.contents == '' && master_frame.contents != '') {
